808efc51a2fda706f11483b1de982488befd7462
Manit Kant <manit_kant@yahoo.com>
2021-04-01 19:17:52 +0530

    basic app using all libs and running the app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('As usual, Hello world!');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+const ads = [{
+    title: 'Hello, world again!',    
+}]
+
+//for API security
+app.use(helmet());
+
+//to parse JSON objects
+app.use(bodyParser.json());
+
+//to enable CORS
+app.use(cors());
+
+//for logging HTTP requests
+app.use(morgan('combined'));
+
+app.get('/', (req,res) => {
+    res.send(ads);
+});
+
+app.listen(3001, () => {
+    console.log('server running at port 3001');
+})
+
